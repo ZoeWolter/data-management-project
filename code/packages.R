b7fc0deb70d1e48a6f05911ba9d3c120f25a4f4f
ZoeWolter <zoe.l.wolter@gmail.com>
2023-08-30 16:52:19 +0200

    packages and knit

---
+++ code/packages.R
@@ -0,0 +1,14 @@
+# install pacman if necessary
+suppressWarnings(if (!require("pacman")) install.packages("pacman"))
+
+# load all packages
+pacman::p_load(tidyverse,
+               RPostgres,
+               sf,
+               countrycode,
+               install = TRUE,
+               update = FALSE)
+
+# print loaded packages
+cat("loaded packages\n")
+print(pacman::p_loaded())
